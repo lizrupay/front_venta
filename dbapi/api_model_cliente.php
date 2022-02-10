035eb3192e08b929e3b5a069c2d789682ed85720
lizrupay <98402198+lizrupay@users.noreply.github.com>
2022-02-09 20:22:13 -0500

    Add files via upload

---
+++ dbapi/api_model_cliente.php
@@ -0,0 +1,402 @@
+<?php
+class api_model_cliente{
+
+
+
+		//-------- Tabla Cliente      --------
+		//--------------------------------------------------	
+
+    
+    public function cliente_buscar($p_codigo){
+
+  
+	
+        $datojson = json_encode("");
+
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/cliente/buscar/'.$p_codigo;
+
+        $curl = curl_init($rludato);
+        
+
+
+        curl_setopt_array($curl, array(
+            // Indicar que vamos a hacer una petición POST
+            CURLOPT_CUSTOMREQUEST => "GET",
+            // Justo aquí ponemos los datos dentro del cuerpo
+            CURLOPT_POSTFIELDS => $datojson,
+            // Encabezados
+            //CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($datojson), // Abajo podríamos agregar más encabezados
+                'Personalizado: ¡Hola mundo!', # Un encabezado personalizado
+            ),
+            # indicar que regrese los datos, no que los imprima directamente
+            CURLOPT_RETURNTRANSFER => true,
+        ));
+
+
+        # Hora de hacer la petición
+        $resultado = curl_exec($curl);
+        # Vemos si el código es 200, es decir, HTTP_OK
+        $codigoRespuesta = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $arrayR = $resultado;
+       
+
+       
+
+		return $arrayR;
+
+    }  
+    
+     
+    public function cliente_eliminar($p_codigo){
+
+  
+	
+        $datojson = json_encode("");
+
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/cliente/borrar/'.$p_codigo;
+
+        $curl = curl_init($rludato);
+        
+
+
+        curl_setopt_array($curl, array(
+            // Indicar que vamos a hacer una petición POST
+            CURLOPT_CUSTOMREQUEST => "DELETE",
+            // Justo aquí ponemos los datos dentro del cuerpo
+            CURLOPT_POSTFIELDS => $datojson,
+            // Encabezados
+            //CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($datojson), // Abajo podríamos agregar más encabezados
+                'Personalizado: ¡Hola mundo!', # Un encabezado personalizado
+            ),
+            # indicar que regrese los datos, no que los imprima directamente
+            CURLOPT_RETURNTRANSFER => true,
+        ));
+
+
+        # Hora de hacer la petición
+        $resultado = curl_exec($curl);
+        # Vemos si el código es 200, es decir, HTTP_OK
+        $codigoRespuesta = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $arrayR = $resultado;
+       
+		return $arrayR;
+
+    }  
+
+    public function cliente_editar($p_info_json){
+
+  
+        $p_codigo = $p_info_json['cliente_id'];
+        $datojson = json_encode($p_info_json);
+
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/cliente/editar/'.$p_codigo;
+
+
+        $curl = curl_init($rludato);
+        
+
+
+        curl_setopt_array($curl, array(
+            // Indicar que vamos a hacer una petición POST
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            // Justo aquí ponemos los datos dentro del cuerpo
+            CURLOPT_POSTFIELDS => $datojson,
+            // Encabezados
+            //CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($datojson), // Abajo podríamos agregar más encabezados
+                'Personalizado: ¡Hola mundo!', # Un encabezado personalizado
+            ),
+            # indicar que regrese los datos, no que los imprima directamente
+            CURLOPT_RETURNTRANSFER => true,
+        ));
+
+
+        # Hora de hacer la petición
+        $resultado = curl_exec($curl);
+        # Vemos si el código es 200, es decir, HTTP_OK
+        $codigoRespuesta = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $arrayR = $resultado;
+       
+
+        $respuestaDecodificada = json_decode($arrayR);
+
+       
+      //  print_r("codigo de respuesta".$codigoRespuesta);
+       // print_r("json de respuesta".$respuestaDecodificada);
+       
+
+
+
+		return $arrayR;
+
+    }
+
+
+    public function cliente_registrar($p_info_json){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/cliente/registrar';
+
+
+        $curl = curl_init($rludato);
+        $datojson = json_encode($p_info_json);
+
+
+        curl_setopt_array($curl, array(
+            // Indicar que vamos a hacer una petición POST
+            CURLOPT_CUSTOMREQUEST => "POST",
+            // Justo aquí ponemos los datos dentro del cuerpo
+            CURLOPT_POSTFIELDS => $datojson,
+            // Encabezados
+            //CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($datojson), // Abajo podríamos agregar más encabezados
+                'Personalizado: ¡Hola mundo!', # Un encabezado personalizado
+            ),
+            # indicar que regrese los datos, no que los imprima directamente
+            CURLOPT_RETURNTRANSFER => true,
+        ));
+
+
+        # Hora de hacer la petición
+        $resultado = curl_exec($curl);
+        # Vemos si el código es 200, es decir, HTTP_OK
+        $codigoRespuesta = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $arrayR = $resultado;
+       
+
+        $respuestaDecodificada = json_decode($arrayR);
+
+       
+      //  print_r("codigo de respuesta".$codigoRespuesta);
+       // print_r("json de respuesta".$respuestaDecodificada);
+       
+
+
+
+		return $arrayR;
+
+    }
+
+    public function cliente_listar(){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/cliente/listar';
+
+		$url = str_replace(' ', '%20', $rludato);
+        $url = str_replace('"', '', $url);
+        $url = str_replace("'", '', $url);
+		$url_new = rawurlencode($rludato);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+		$arrayR = $response; //json_decode($response, true);
+		
+		/*
+		$pcadena = " >> url >> ".$url."\r\n";
+		$pcadena = $pcadena." >> urlnew >> ".$url_new."\r\n";
+		$pcadena = $pcadena." >> rpta >> ".$arrayR;
+		
+		Sp_GuardarLogAGI($pcadena);	
+		*/
+		return $arrayR;
+
+    }    
+
+
+
+    public function cliente_listar_porid($p_campo_dato){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/cliente/listar_id/'.$p_campo_dato;
+
+		$url = str_replace(' ', '%20', $rludato);
+        $url = str_replace('"', '', $url);
+        $url = str_replace("'", '', $url);
+		$url_new = rawurlencode($rludato);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+		$arrayR = $response; //json_decode($response, true);
+		
+		/*
+		$pcadena = " >> url >> ".$url."\r\n";
+		$pcadena = $pcadena." >> urlnew >> ".$url_new."\r\n";
+		$pcadena = $pcadena." >> rpta >> ".$arrayR;
+		
+		Sp_GuardarLogAGI($pcadena);	
+		*/
+		return $arrayR;
+
+    }   
+
+
+    public function cliente_listar_pornombre($p_campo_dato){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/cliente/listar_nombre/'.$p_campo_dato;
+
+		$url = str_replace(' ', '%20', $rludato);
+        $url = str_replace('"', '', $url);
+        $url = str_replace("'", '', $url);
+		$url_new = rawurlencode($rludato);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+		$arrayR = $response; //json_decode($response, true);
+		
+		/*
+		$pcadena = " >> url >> ".$url."\r\n";
+		$pcadena = $pcadena." >> urlnew >> ".$url_new."\r\n";
+		$pcadena = $pcadena." >> rpta >> ".$arrayR;
+		
+		Sp_GuardarLogAGI($pcadena);	
+		*/
+		return $arrayR;
+
+    }        
+	
+    public function cliente_listar_porapellido($p_campo_dni){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/cliente/listar_apellido/'.$p_campo_dni;
+
+		$url = str_replace(' ', '%20', $rludato);
+        $url = str_replace('"', '', $url);
+        $url = str_replace("'", '', $url);
+		$url_new = rawurlencode($rludato);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+		$arrayR = $response; //json_decode($response, true);
+		
+		/*
+		$pcadena = " >> url >> ".$url."\r\n";
+		$pcadena = $pcadena." >> urlnew >> ".$url_new."\r\n";
+		$pcadena = $pcadena." >> rpta >> ".$arrayR;
+		
+		Sp_GuardarLogAGI($pcadena);	
+		*/
+		return $arrayR;
+
+    }   
+
+    public function cliente_listar_pordni($p_campo_dni){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/cliente/listar_dni/'.$p_campo_dni;
+
+		$url = str_replace(' ', '%20', $rludato);
+        $url = str_replace('"', '', $url);
+        $url = str_replace("'", '', $url);
+		$url_new = rawurlencode($rludato);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+		$arrayR = $response; //json_decode($response, true);
+		
+		/*
+		$pcadena = " >> url >> ".$url."\r\n";
+		$pcadena = $pcadena." >> urlnew >> ".$url_new."\r\n";
+		$pcadena = $pcadena." >> rpta >> ".$arrayR;
+		
+		Sp_GuardarLogAGI($pcadena);	
+		*/
+		return $arrayR;
+
+    }        
+
+
+
+}
+
+?>
---
+++ dbapi/api_model_usuario.php
@@ -0,0 +1,288 @@
+<?php
+class api_model_usuario{
+
+
+
+		//-------- Tabla Categoria       --------
+		//--------------------------------------------------	
+
+    public function usuario_buscar($p_codigo){
+
+  
+	
+        $datojson = json_encode("");
+
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/usuario/buscar/'.$p_codigo;
+
+        $curl = curl_init($rludato);
+        
+
+
+        curl_setopt_array($curl, array(
+            // Indicar que vamos a hacer una petición POST
+            CURLOPT_CUSTOMREQUEST => "GET",
+            // Justo aquí ponemos los datos dentro del cuerpo
+            CURLOPT_POSTFIELDS => $datojson,
+            // Encabezados
+            //CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($datojson), // Abajo podríamos agregar más encabezados
+                'Personalizado: ¡Hola mundo!', # Un encabezado personalizado
+            ),
+            # indicar que regrese los datos, no que los imprima directamente
+            CURLOPT_RETURNTRANSFER => true,
+        ));
+
+
+        # Hora de hacer la petición
+        $resultado = curl_exec($curl);
+        # Vemos si el código es 200, es decir, HTTP_OK
+        $codigoRespuesta = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $arrayR = $resultado;
+         
+
+		return $arrayR;
+
+    }  
+    
+
+
+    public function usuario_validarlogin($p_info_json){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/usuario/validarlogin';
+
+
+        $curl = curl_init($rludato);
+        $datojson = json_encode($p_info_json);
+
+
+        curl_setopt_array($curl, array(
+            // Indicar que vamos a hacer una petición POST
+            CURLOPT_CUSTOMREQUEST => "POST",
+            // Justo aquí ponemos los datos dentro del cuerpo
+            CURLOPT_POSTFIELDS => $datojson,
+            // Encabezados
+            //CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($datojson), // Abajo podríamos agregar más encabezados
+                'Personalizado: ¡Hola mundo!', # Un encabezado personalizado
+            ),
+            # indicar que regrese los datos, no que los imprima directamente
+            CURLOPT_RETURNTRANSFER => true,
+        ));
+
+
+        # Hora de hacer la petición
+        $resultado = curl_exec($curl);
+        # Vemos si el código es 200, es decir, HTTP_OK
+        $codigoRespuesta = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $arrayR = $resultado;
+       
+
+        $respuestaDecodificada = json_decode($arrayR);
+
+       
+      //  print_r("codigo de respuesta".$codigoRespuesta);
+       // print_r("json de respuesta".$respuestaDecodificada);
+       
+
+
+
+		return $arrayR;
+
+    }
+
+
+    public function usuario_editar($p_info_json){
+
+  
+        $p_codigo = $p_info_json['usuario_id'];
+        $datojson = json_encode($p_info_json);
+
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/usuario/editar/'.$p_codigo;
+
+
+        $curl = curl_init($rludato);
+        
+
+
+        curl_setopt_array($curl, array(
+            // Indicar que vamos a hacer una petición POST
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            // Justo aquí ponemos los datos dentro del cuerpo
+            CURLOPT_POSTFIELDS => $datojson,
+            // Encabezados
+            //CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($datojson), // Abajo podríamos agregar más encabezados
+                'Personalizado: ¡Hola mundo!', # Un encabezado personalizado
+            ),
+            # indicar que regrese los datos, no que los imprima directamente
+            CURLOPT_RETURNTRANSFER => true,
+        ));
+
+
+        # Hora de hacer la petición
+        $resultado = curl_exec($curl);
+        # Vemos si el código es 200, es decir, HTTP_OK
+        $codigoRespuesta = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $arrayR = $resultado;
+       
+
+        $respuestaDecodificada = json_decode($arrayR);
+
+       
+      //  print_r("codigo de respuesta".$codigoRespuesta);
+       // print_r("json de respuesta".$respuestaDecodificada);
+       
+
+		return $arrayR;
+
+    }
+
+
+    public function usuario_registrar($p_info_json){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/usuario/registrar';
+
+
+        $curl = curl_init($rludato);
+        $datojson = json_encode($p_info_json);
+
+
+        curl_setopt_array($curl, array(
+            // Indicar que vamos a hacer una petición POST
+            CURLOPT_CUSTOMREQUEST => "POST",
+            // Justo aquí ponemos los datos dentro del cuerpo
+            CURLOPT_POSTFIELDS => $datojson,
+            // Encabezados
+            //CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($datojson), // Abajo podríamos agregar más encabezados
+                'Personalizado: ¡Hola mundo!', # Un encabezado personalizado
+            ),
+            # indicar que regrese los datos, no que los imprima directamente
+            CURLOPT_RETURNTRANSFER => true,
+        ));
+
+
+        # Hora de hacer la petición
+        $resultado = curl_exec($curl);
+        # Vemos si el código es 200, es decir, HTTP_OK
+        $codigoRespuesta = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        $arrayR = $resultado;
+       
+
+        $respuestaDecodificada = json_decode($arrayR);
+
+       
+      //  print_r("codigo de respuesta".$codigoRespuesta);
+       // print_r("json de respuesta".$respuestaDecodificada);
+       
+
+
+
+		return $arrayR;
+
+    }
+
+    public function usuario_listar(){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/usuario/listar';
+
+		$url = str_replace(' ', '%20', $rludato);
+        $url = str_replace('"', '', $url);
+        $url = str_replace("'", '', $url);
+		$url_new = rawurlencode($rludato);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+		$arrayR = $response; //json_decode($response, true);
+		
+		/*
+		$pcadena = " >> url >> ".$url."\r\n";
+		$pcadena = $pcadena." >> urlnew >> ".$url_new."\r\n";
+		$pcadena = $pcadena." >> rpta >> ".$arrayR;
+		
+		Sp_GuardarLogAGI($pcadena);	
+		*/
+		return $arrayR;
+
+    }    
+
+
+
+
+    public function usuario_listar_pordescripcion($p_campo_dato){
+
+  
+	
+        $rludato = 'http://172.96.143.27:8085/proyecto/tecnomarket/usuario/listar_descripcion/'.$p_campo_dato;
+
+		$url = str_replace(' ', '%20', $rludato);
+        $url = str_replace('"', '', $url);
+        $url = str_replace("'", '', $url);
+		$url_new = rawurlencode($rludato);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+		$arrayR = $response; //json_decode($response, true);
+		
+		/*
+		$pcadena = " >> url >> ".$url."\r\n";
+		$pcadena = $pcadena." >> urlnew >> ".$url_new."\r\n";
+		$pcadena = $pcadena." >> rpta >> ".$arrayR;
+		
+		Sp_GuardarLogAGI($pcadena);	
+		*/
+		return $arrayR;
+
+    }   
+    
+
+
+}
+
+?>
